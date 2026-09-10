3735c83266cc41d98435b486299028f2252516ad
Federico Malfasi <federicomalfasi@Mac-mini-2.local>
2026-09-10 13:28:25 -0300

    Todo Comments Add 'DONE'

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/todo-comments.nvim",
+  opts = {
+    keywords = {
+      DONE = {
+        icon = "✅",
+        color = "#28a745",
+        alt = { "COMPLETE", "RESOLVED" },
+      },
+    },
+  },
+}
