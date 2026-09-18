9ca656ef7dea5a5a18e7babd63b80890193362ba
Federico Malfasi <federicomalfasi@Mac-mini-2.local>
2026-09-18 20:39:01 -0300

    Add blink to autocomplete import in brackets

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
+      ["<C-l>"] = { "show", "hide" },
+    },
+  },
+}
